05abdfa609b27ed0388f3485dde9c855eb513fa6
Auston Barboza <austonpramodh@gmail.com>
2023-06-06 11:46:25 -0500

    chore: redux setup complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,38 @@
-import React, { ReactElement } from "react";
+import React from "react";
+import { Provider } from "react-redux";
 import { PaperProvider } from "react-native-paper";
 import { NavigationContainer, NavigationContainerRef } from "@react-navigation/native";
 
 import { LightTheme } from "./theme";
 import Navigator, { RootStackParamList } from "./Navigator";
+import { RootStore } from "./store";
 
-function App(): ReactElement {
+type AppProps = {
+    store: RootStore;
+};
+
+const App: React.FunctionComponent<AppProps> = ({ store }) => {
     const intializeApp = async (navigationContainerRef: NavigationContainerRef<RootStackParamList>) => {
         // eslint-disable-next-line no-console
         console.log(navigationContainerRef);
     };
 
     return (
-        <PaperProvider theme={LightTheme}>
-            <NavigationContainer<RootStackParamList>
-                ref={(container) => {
-                    if (container)
-                        // run the following function after main thread is free
-                        setImmediate(() => intializeApp(container));
-                }}
-            >
-                <Navigator />
-            </NavigationContainer>
-        </PaperProvider>
+        <Provider store={store}>
+            <PaperProvider theme={LightTheme}>
+                <NavigationContainer<RootStackParamList>
+                    ref={(container) => {
+                        if (container)
+                            // run the following function after main thread is free
+                            setImmediate(() => intializeApp(container));
+                    }}
+                >
+                    <Navigator />
+                </NavigationContainer>
+            </PaperProvider>
+        </Provider>
     );
-}
-
+};
 // const styles = StyleSheet.create({
 //     sectionContainer: {
 //         marginTop: 32,
