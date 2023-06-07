d952b9894b4fa6a23169e8db090db5226975040d
Auston Barboza <austonpramodh@gmail.com>
2023-06-07 14:44:40 -0500

    chore: husky implemented for commit checks

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -2,13 +2,17 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
-import App from '../src/App';
+import "react-native";
+import React from "react";
+import renderer from "react-test-renderer";
+
+import App from "../src/App";
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+it("renders correctly", () => {
+    // TODO: this ts-ignore is a hack to get around the fact that we are not doing any tests
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    renderer.create(<App />);
 });
